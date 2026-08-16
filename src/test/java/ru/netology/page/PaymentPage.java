5d1d8bad4a88a8f43ae460e6335325015372e510
K <ktokar06@mail.ru>
2026-08-16 17:36:28 +0300

    Update Code

--- src/test/java/ru/netology/page/PaymentPage.java
+++ src/test/java/ru/netology/page/PaymentPage.java
@@ -2,6 +2,7 @@ package ru.netology.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import ru.netology.data.CardInfo;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -18,6 +19,7 @@ public class PaymentPage {
             .first();
     private final SelenideElement successNotification = $(".notification_status_ok");
 
+    @Step("Заполнение формы оплаты картой: номер={cardInfo.number}, месяц={cardInfo.month}, год={cardInfo.year}, держатель={cardInfo.holder}")
     public void fillForm(CardInfo cardInfo) {
         cardNumberField.setValue(cardInfo.getNumber());
         monthField.setValue(cardInfo.getMonth());
@@ -27,6 +29,7 @@ public class PaymentPage {
         continueButton.click();
     }
 
+    @Step("Проверка успешного уведомления об оплате")
     public void verifySuccessNotification() {
         successNotification.shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Успешно"));
