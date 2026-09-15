f83284d33d1817c53106f750adb22f43d70d2bb3
K <ktokar06@mail.ru>
2026-09-15 23:14:15 +0300

    Update Code

--- src/test/java/ru/netology/page/CreditPage.java
+++ src/test/java/ru/netology/page/CreditPage.java
@@ -5,8 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import ru.netology.data.CardInfo;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -48,6 +46,6 @@ public class CreditPage {
         $$(".notification")
                 .findBy(Condition.visible)
                 .find(".notification__title")
-                .shouldHave(Condition.text(expectedText), Duration.ofSeconds(15));
+                .shouldHave(Condition.text(expectedText));
     }
 }
